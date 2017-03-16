96d7545a0c5d9d07c56a8df5e39ab02222b233c1
Emilie <emiliehester@gmail.com>
2017-03-16 15:37:51 -0600

    Setup migrations, models, edit config.json

--- server/migrations/20170316210715-create-tag.js
+++ server/migrations/20170316210715-create-tag.js
@@ -12,6 +12,11 @@ module.exports = {
         allowNull: false,
         type: Sequelize.STRING
       },
+      ResourceId: {
+        allowNull: false,
+        unique: true,
+        type: Sequelize.INTEGER
+      },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE
