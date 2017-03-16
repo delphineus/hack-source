abcd397be6069849c3c794e44ced73ffd84102a3
Max Berger <maxamillianberger@gmail.com>
2017-03-16 14:57:35 -0700

    Add routes for resources and users

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,7 +1,21 @@
 var resourceController = require('../resources/resourceController.js');
+var userController = require('../users/userController.js');
 
 module.exports = function(app, express) {
   // resources
   app.get('/api/resources', resourceController.allResources);
   app.post('/api/resources', resourceController.newResource);
+  app.get('/api/resources/category', resourceController.byCategory);
+  app.get('/api/resources/tag', resourceController.byTag);
+
+  app.post('/api/likes', resourceController.likes);
+  app.get('/api/categories', resourceController.allCategories);
+  app.get('/api/tags', resourceController.allTags);
+  app.get('/api/user/bookmarks', resourceController.bookmarks);
+
+  // users
+  app.get('/api/users', userController.checkAuth);
+  // not sure about this one...
+  app.get('/api/users/login', userController.login);
+  app.post('/api/users', userController.signup);
 };
