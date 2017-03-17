33851755cce8bb28427e8b5ba3acd432d1d60812
Emilie <emiliehester@gmail.com>
2017-03-16 19:34:57 -0600

    Updated Model Like associations

--- server/models/like.js
+++ server/models/like.js
@@ -6,7 +6,8 @@ module.exports = function(sequelize, DataTypes) {
   }, {
     classMethods: {
       associate: function(models) {
-        // associations can be defined here
+        Like.belongsTo(models.Resource);
+        Like.belongsTo(models.User);
       }
     }
   });
