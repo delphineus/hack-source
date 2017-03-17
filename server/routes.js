56209c286f7d2427f612eecfb41829feb3817818
Emilie <emiliehester@gmail.com>
2017-03-16 19:41:21 -0600

    Move routes from config folder to server foot

--- server/routes.js
+++ server/routes.js
@@ -1,22 +1,21 @@
-var resourceController = require('./controllers/resourceController.js');
-var userController = require('./controllers/userController.js');
-var router = require('express').Router();
+var resourceController = require('../resources/resourceController.js');
+var userController = require('../users/userController.js');
 
-// resources
-router.get('/resources', resourceController.getResources);
-router.post('/resources', resourceController.postResource);
-router.get('/resources/category', resourceController.getCategory);
-router.get('/resources/tag', resourceController.getTag);
+module.exports = function(app, express) {
+  // resources
+  app.get('/api/resources', resourceController.getResources);
+  app.post('/api/resources', resourceController.postResource);
+  app.get('/api/resources/category', resourceController.getCategory);
+  app.get('/api/resources/tag', resourceController.getTag);
 
-router.post('/likes', resourceController.postLikes);
-router.get('/categories', resourceController.getCategories);
-router.get('/tags', resourceController.getTags);
-router.get('/user/bookmarks', resourceController.getBookmarks);
+  app.post('/api/likes', resourceController.postLikes);
+  app.get('/api/categories', resourceController.getCategories);
+  app.get('/api/tags', resourceController.getTags);
+  app.get('/api/user/bookmarks', resourceController.getBookmarks);
 
-// users
-router.get('/users', userController.checkAuth);
-router.get('/users/login', userController.login);
-router.get('/users/logout', userController.logout);
-router.post('/users', userController.signup);
-
-module.exports = router;
+  // users
+  app.get('/api/users/', userController.checkAuth);
+  app.get('/api/users/login', userController.login);
+  app.get('/api/users/logout', userController.logout);
+  app.post('/api/users', userController.signup);
+};
