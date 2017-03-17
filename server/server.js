77b73d4a5b35912351a3c4425f42890bc2420d0b
Max Berger <maxamillianberger@gmail.com>
2017-03-17 10:40:50 -0700

    Fix middleware

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '/')));
 app.use('/api', routes);
 
-app.get('/api/test', function(req, res) {
+app.get('/', function(req, res) {
   res.send('Hello, World!');
 });
 
