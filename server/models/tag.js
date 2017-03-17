0fa1c9af24a99792a21495158a161cf987b89a3b
Emilie <emiliehester@gmail.com>
2017-03-17 13:37:29 -0600

    Fix resource tag and resource category relationships

--- server/models/tag.js
+++ server/models/tag.js
@@ -1,12 +1,10 @@
 'use strict';
 module.exports = function(sequelize, DataTypes) {
   var Tag = sequelize.define('Tag', {
-    title: DataTypes.STRING,
-    ResourceId: DataTypes.INTEGER
+    title: DataTypes.STRING
   }, {
     classMethods: {
       associate: function(models) {
-        // Tag.belongsTo(models.Resource);
         Tag.belongsToMany(models.Resource, {through: models.ResourceTag});
       }
     }
