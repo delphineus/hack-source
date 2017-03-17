a626e5c7e450ba7605cf114772d575b69d9fe1db
Emilie <emiliehester@gmail.com>
2017-03-16 19:49:00 -0600

    Add route path and use to middleware

--- server/middleware.js
+++ server/middleware.js
@@ -1,9 +1,11 @@
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+var routes = require('./routes');
 
 module.exports = function(app, express) {
   app.use(morgan('dev'));
   app.use(bodyParser.urlencoded({extended: true}));
   app.use(bodyParser.json());
   app.use(express.static(path.join(__dirname, '/')));
+  app.use('/api', routes);
 };
