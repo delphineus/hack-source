c5f76485ba0574110669f452f954e725aa1b287f
Emilie <emiliehester@gmail.com>
2017-03-16 19:36:04 -0600

    Add missing semicolons

--- server/middleware.js
+++ server/middleware.js
@@ -1,5 +1,5 @@
-const morgan = require('morgan')
-const bodyParser = require('body-parser')
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 module.exports = function(app, express) {
   app.use(morgan('dev'));
