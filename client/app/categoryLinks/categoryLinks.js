773a4c1dbf48bdb56ca66009728b98c928fcd93a
Nate Nault <nate@nathannault.com>
2017-03-25 06:21:45 +0800

    Create categoryLink module and markup

---
+++ client/app/categoryLinks/categoryLinks.js
@@ -0,0 +1,40 @@
+angular.module('hackSource.categoryLinks', [])
+.factory('Cats', function($http) {
+
+  var getAllCats = function () {
+    return $http({
+      method: 'GET',
+      url: '/api/categories'
+    })
+    .then(function (resp) {
+      return resp.data;
+    });
+  };
+
+  return {
+    getAllCats: getAllCats
+  };
+
+})
+.controller('CatLinksCtrl', function($scope, Cats) {
+
+  var initializeCats = function () {
+    Cats.getAllCats()
+      .then(function (cats) {
+        $scope.categories = cats;
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  };
+
+  initializeCats();
+
+})
+.directive('catlinks', function() {
+  return {
+    // restrict: 'E',
+    templateUrl: 'app/categoryLinks/categoryLinks.html',
+    controller: 'CatLinksCtrl'
+  };
+});
