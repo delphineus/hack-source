4ca922b3886dec75ca9123d0cd42bfb575adf07a
Nate Nault <nate@nathannault.com>
2017-03-24 09:14:31 +0800

    Add orderBy variables and logic/function to cardList controller

--- client/app/cardList/cardList.js
+++ client/app/cardList/cardList.js
@@ -1,6 +1,17 @@
 angular.module('hackSource.cardList', [])
 .controller('cardListCtrl', function($scope, Data) {
 
+  // set the default sort type
+  $scope.sortType = 'createdAt';
+  // set the default sort order
+  $scope.sortReverse = false;
+
+  // Sort functionality
+  $scope.sortBy = function(sortParam) {
+    $scope.sortType = sortParam;
+    $scope.sortReverse = !$scope.sortReverse;
+  };
+
 })
 .directive('myCardList', function() {
   return {
