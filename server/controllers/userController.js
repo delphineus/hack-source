753886da1d7d03bb50e5dfcb365c0ff5850eb510
Emilie <emiliehester@gmail.com>
2017-03-26 14:22:03 -0600

    Revert config, userController, user-seeder

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -3,15 +3,11 @@ var User = require('../models').User;
 module.exports = {
 
   checkAuthCallback: function(req, res) {
-    res.redirect('/logged-in');
-  },
-
-  login: function(req, res) {
-    res.send('Please login.'); // TODO send to Angular login view
+    res.redirect('/');
   },
 
   logout: function(req, res) {
     req.logout();
-    res.redirect('/api/login');
+    res.redirect('/');
   }
 };
