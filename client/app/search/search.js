ec03eb474bf600d5115c615de67cbf1b9c3f3597
Jordan Stubblefield <JStubblefield7939@gmail.com>
2017-03-29 23:55:01 -0400

    Enable live search and clean up search.html/js and services.js

--- client/app/search/search.js
+++ client/app/search/search.js
@@ -1,41 +1,6 @@
 angular.module('hackSource.search', [])
 .controller('searchCtrl', function($scope, $http, $q, $timeout, Data) {
-  // $scope.queryLibrary = [];
-  // Data.getAllCategories().then(resp => {
-  //   resp.forEach(item => {
-  //     $scope.queryLibrary.push(item);
-  //   });
-  // });
-  // Data.getAllTags().then(resp => {
-  //   resp.forEach(item => {
-  //     $scope.queryLibrary.push(item);
-  //   });
-  // });
-  //
-  // $scope.selectedItem = null;
-  // $scope.searchText = null;
-  //
-  // $scope.querySearch = querySearch;
-  //
-  // function querySearch (query) {
-  //   var results = query ? $scope.queryLibrary.filter( createFilterFor(query) ) : $scope.queryLibrary;
-  //   var deferred = $q.defer();
-  //   $timeout(function () { deferred.resolve( results ); }, Math.random() * 1000, false);
-  //   return deferred.promise;
-  // }
-  //
-  // function createFilterFor(query) {
-  //   var lowercaseQuery = angular.lowercase(query);
-  //   return function filterFn(cat) {
-  //     return (cat.title.indexOf(lowercaseQuery) === 0);
-  //   };
-  //
-  // }
-  //
-  // $scope.clearInput = function() {
-  //   $scope.selectedItem = null;
-  //   $scope.searchText = null;
-  // };
+
 })
 .directive('hsSearch', function() {
   return {
