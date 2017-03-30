1ad85dea66d3efa323cb4d47edfc4e7c4a6e809d
Jordan Stubblefield <JStubblefield7939@gmail.com>
2017-03-29 23:47:31 -0400

    Refactor search control and services to remove old search methods

--- client/app/services/services.js
+++ client/app/services/services.js
@@ -160,156 +160,156 @@ angular.module('hackSource.services', [])
     }
     return filtered;
   };
-})
-.filter('filterBySearch', function () {
-  return function (items, searchBar) {
-    var filtered = [];
-
-    if (!searchBar) {
-      return items;
-    }
-
-    for (var i = 0; i < items.length; i++) {
-      var item = items[i];
-      var itemSearch = [];
-
-      item.Tags.forEach(function(tag) {
-        itemSearch.push(tag.title);
-      });
-      item.Categories.forEach(function(cat) {
-        itemSearch.push(cat.title);
-      });
-      item.title.toLowerCase().split(' ').forEach(function(word) {
-        if (!stopWords[word]) itemSearch.push(word);
-      });
-
-      if (itemSearch.indexOf(searchBar) !== -1) {
-        filtered.push(item);
-      }
-    }
-    return filtered;
-  };
 });
+// .filter('filterBySearch', function () {
+//   return function (items, searchBar) {
+//     var filtered = [];
+//
+//     if (!searchBar) {
+//       return items;
+//     }
+//
+//     for (var i = 0; i < items.length; i++) {
+//       var item = items[i];
+//       var itemSearch = [];
+//
+//       item.Tags.forEach(function(tag) {
+//         itemSearch.push(tag.title);
+//       });
+//       item.Categories.forEach(function(cat) {
+//         itemSearch.push(cat.title);
+//       });
+//       item.title.toLowerCase().split(' ').forEach(function(word) {
+//         if (!stopWords[word]) itemSearch.push(word);
+//       });
+//
+//       if (itemSearch.indexOf(searchBar) !== -1) {
+//         filtered.push(item);
+//       }
+//     }
+//     return filtered;
+//   };
+// });
 
 //these common words are excluded from the above search
-const stopWords = {
-a: true,
-able: true,
-about: true,
-across: true,
-after: true,
-all: true,
-almost: true,
-also: true,
-am: true,
-among: true,
-an: true,
-and: true,
-any: true,
-are: true,
-as: true,
-at: true,
-be: true,
-because: true,
-been: true,
-but: true,
-by: true,
-can: true,
-cannot: true,
-could: true,
-dear: true,
-did: true,
-do: true,
-does: true,
-either: true,
-else: true,
-ever: true,
-every: true,
-for: true,
-from: true,
-get: true,
-got: true,
-had: true,
-has: true,
-have: true,
-he: true,
-her: true,
-hers: true,
-him: true,
-his: true,
-how: true,
-however: true,
-i: true,
-if: true,
-in: true,
-into: true,
-is: true,
-it: true,
-its: true,
-just: true,
-least: true,
-let: true,
-like: true,
-likely: true,
-may: true,
-me: true,
-might: true,
-most: true,
-must: true,
-my: true,
-neither: true,
-no: true,
-nor: true,
-not: true,
-of: true,
-off: true,
-often: true,
-on: true,
-only: true,
-or: true,
-other: true,
-our: true,
-own: true,
-rather: true,
-said: true,
-say: true,
-says: true,
-she: true,
-should: true,
-since: true,
-so: true,
-some: true,
-than: true,
-that: true,
-the: true,
-their: true,
-them: true,
-then: true,
-there: true,
-these: true,
-they: true,
-this: true,
-tis: true,
-to: true,
-too: true,
-twas: true,
-us: true,
-wants: true,
-was: true,
-we: true,
-were: true,
-what: true,
-when: true,
-where: true,
-which: true,
-while: true,
-who: true,
-whom: true,
-why: true,
-will: true,
-with: true,
-would: true,
-yet: true,
-you: true,
-your: true
-};
+// const stopWords = {
+// a: true,
+// able: true,
+// about: true,
+// across: true,
+// after: true,
+// all: true,
+// almost: true,
+// also: true,
+// am: true,
+// among: true,
+// an: true,
+// and: true,
+// any: true,
+// are: true,
+// as: true,
+// at: true,
+// be: true,
+// because: true,
+// been: true,
+// but: true,
+// by: true,
+// can: true,
+// cannot: true,
+// could: true,
+// dear: true,
+// did: true,
+// do: true,
+// does: true,
+// either: true,
+// else: true,
+// ever: true,
+// every: true,
+// for: true,
+// from: true,
+// get: true,
+// got: true,
+// had: true,
+// has: true,
+// have: true,
+// he: true,
+// her: true,
+// hers: true,
+// him: true,
+// his: true,
+// how: true,
+// however: true,
+// i: true,
+// if: true,
+// in: true,
+// into: true,
+// is: true,
+// it: true,
+// its: true,
+// just: true,
+// least: true,
+// let: true,
+// like: true,
+// likely: true,
+// may: true,
+// me: true,
+// might: true,
+// most: true,
+// must: true,
+// my: true,
+// neither: true,
+// no: true,
+// nor: true,
+// not: true,
+// of: true,
+// off: true,
+// often: true,
+// on: true,
+// only: true,
+// or: true,
+// other: true,
+// our: true,
+// own: true,
+// rather: true,
+// said: true,
+// say: true,
+// says: true,
+// she: true,
+// should: true,
+// since: true,
+// so: true,
+// some: true,
+// than: true,
+// that: true,
+// the: true,
+// their: true,
+// them: true,
+// then: true,
+// there: true,
+// these: true,
+// they: true,
+// this: true,
+// tis: true,
+// to: true,
+// too: true,
+// twas: true,
+// us: true,
+// wants: true,
+// was: true,
+// we: true,
+// were: true,
+// what: true,
+// when: true,
+// where: true,
+// which: true,
+// while: true,
+// who: true,
+// whom: true,
+// why: true,
+// will: true,
+// with: true,
+// would: true,
+// yet: true,
+// you: true,
+// your: true
+// };
