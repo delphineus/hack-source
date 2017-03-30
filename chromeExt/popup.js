10b5d5f7591c3d87345a0700cfe6ffc39d7961a2
lmegviar <lmegviar@gmail.com>
2017-03-30 11:40:56 -0700

    Create onSubmit function to capture resource data and include og image link

--- chromeExt/popup.js
+++ chromeExt/popup.js
@@ -11,35 +11,39 @@ function getCurrentTabUrl(callback) {
 
     callback(url);
   });
+};
 
-  // Most methods of the Chrome extension APIs are asynchronous. This means that
-  // you CANNOT do something like this:
-  //
-  // var url;
-  // chrome.tabs.query(queryInfo, function(tabs) {
-  //   url = tabs[0].url;
-  // });
-  // alert(url); // Shows "undefined", because chrome.tabs.query is async.
-}
+var resourceData = {
+  url: '',
+  tags: [],
+  title: '',
+  imgUrl: '',
+  summary: '',
+  userId: '',
+  category: ''
+};
 
-
-function renderURLinput(url) {
-  document.getElementById('urlInput').value = url;
-}
+function onSubmit() {
+  resourceData.url = document.getElementById('urlInput').value;
+  resourceData.title = document.getElementById('titleInput').value;
+  resourceData.summary = document.getElementById('summaryInput').value;
+  // resource.userId =
+  resourceData.category = document.getElementById('categoryInput').value;
+  var tags = document.getElementById('tagsInput').value;
+  resourceData.tags = tags.split(" ");
+};
 
 document.addEventListener('DOMContentLoaded', function() {
   getCurrentTabUrl(function(url) {
-    renderURLinput(url);
-    $.get(url, function(data) {
-        console.log($(data).filter('meta'));
-        console.log($(data).filter('img'));
-        var description = $(data).filter('meta[name=description]').attr('content') || '';
-        var title = $(data).filter('title').text() || '';
-        document.getElementById('summaryInput').value = description;
-        document.getElementById('titleInput').value = title;
-
-    });
-    }, function(errorMessage) {
-      renderStatus('Cannot display image. ' + errorMessage);
+      $.get(url, function(data) {
+          var defaultPhoto = 'https://i.stack.imgur.com/Mmww2.png';
+          resourceData.imgUrl = $(data).filter('meta[property="og:image"]').attr('content') || defaultPhoto;
+          var description = $(data).filter('meta[name=description]').attr('content') || '';
+          var title = $(data).filter('title').text() || '';
+          document.getElementById('urlInput').value = url;
+          document.getElementById('summaryInput').value = description;
+          document.getElementById('titleInput').value = title;
+      });
     });
 });
+
