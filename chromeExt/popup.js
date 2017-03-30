7d8e5c5a3269cf8852182d233ec8747c05aed7c8
lmegviar <lmegviar@gmail.com>
2017-03-30 07:34:14 -0700

    Pull description from site metadata and auto-populate summary field

--- chromeExt/popup.js
+++ chromeExt/popup.js
@@ -1,37 +1,62 @@
-// Copyright (c) 2014 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+// getOpenGraph: function(req, res) {
+//   openGraph(req.body.url, function(err, meta) {
+//     if (err) {
+//       console.error(err);
+//     } else {
+//       console.log('Meta:  ', meta)
+//       res.send(meta);
+//     }
+//   });
+// },
 
-/**
- * Get the current URL.
- *
- * @param {function(string)} callback - called when the URL of the current tab
- *   is found.
- */
+// var getMetaDataFor = function(data) {
+//   return $http({
+//     method: 'POST',
+//     url: '/api/opengraph',
+//     data: JSON.stringify(data)
+//   })
+//   .then(function(resp) {
+//     console.log(resp);
+//     return resp.data;
+//   });
+// };
+
+// getMeta = function(url) {
+//   var data = {
+//     url: url,
+//     imgUrl: 'https://i.stack.imgur.com/Mmww2.png',
+//     category: ''
+//   };
+
+//   // User.checkLoggedIn().then(function(user) {
+//   //   $scope.data.UserId = user.user.id;
+//   // });
+
+//   return Data.getMetaDataFor({url: url})
+//     .then(function(meta) {
+//       console.log(meta);
+//       $scope.data.title = meta.title;
+//       $scope.data.summary = meta.description;
+//       if (meta.image) {
+//         if (Array.isArray(meta.image.url)) {
+//           $scope.data.imgUrl = meta.image.url[meta.image.url.length - 1];
+//         } else {
+//           $scope.data.imgUrl = meta.image.url;
+//         }
+//       }
+//     });
+
+//-----------------------------------------------------------------------------
+
 function getCurrentTabUrl(callback) {
-  // Query filter to be passed to chrome.tabs.query - see
-  // https://developer.chrome.com/extensions/tabs#method-query
   var queryInfo = {
     active: true,
     currentWindow: true
   };
 
   chrome.tabs.query(queryInfo, function(tabs) {
-    // chrome.tabs.query invokes the callback with a list of tabs that match the
-    // query. When the popup is opened, there is certainly a window and at least
-    // one tab, so we can safely assume that |tabs| is a non-empty array.
-    // A window can only have one active tab at a time, so the array consists of
-    // exactly one tab.
     var tab = tabs[0];
-
-    // A tab is a plain object that provides information about the tab.
-    // See https://developer.chrome.com/extensions/tabs#type-Tab
     var url = tab.url;
-
-    // tab.url is only available if the "activeTab" permission is declared.
-    // If you want to see the URL of other tabs (e.g. after removing active:true
-    // from |queryInfo|), then the "tabs" permission is required to see their
-    // "url" properties.
     console.assert(typeof url == 'string', 'tab.url should be a string');
 
     callback(url);
@@ -50,13 +75,19 @@ function getCurrentTabUrl(callback) {
 
 function renderURLinput(url) {
   document.getElementById('urlInput').value = url;
-  console.log('URL in render = ', url);
 }
 
 document.addEventListener('DOMContentLoaded', function() {
   getCurrentTabUrl(function(url) {
-    console.log('URL in currentTabUrl = ', url);
     renderURLinput(url);
+    $.get(url, function(data) {
+        console.log($(data).filter('meta'));
+        console.log($(data).filter('title'));
+        console.log($(data).filter('img'));
+        var description = $(data).filter('meta[name=description]').attr('content')
+        document.getElementById('summaryInput').value = description;
+
+    });
     }, function(errorMessage) {
       renderStatus('Cannot display image. ' + errorMessage);
     });
