f91c4b305208645a0e7f7ec15db5637370c9149c
Kyle Anson <kyle.anson7@gmail.com>
2017-03-29 20:33:40 -0400

    Removed old deployment comments

--- server/githubAuth.js
+++ server/githubAuth.js
@@ -11,11 +11,6 @@ module.exports = passport.use(new GitHubStrategy({
   clientID: process.env.GITHUB_ID || GITHUB.clientID,
   clientSecret: process.env.GITHUB_SECRET || GITHUB.clientSecret,
   callbackURL: process.env.GITHUB_CALLBACK_URL || GITHUB.callbackURL
-
-  /* DEPLOYMENT SETTINGS */
-  // clientID: process.env.GITHUB_ID,
-  // clientSecret: process.env.GITHUB_SECRET,
-  // callbackURL: 'http://glacial-inlet-40419.herokuapp.com/auth/github/callback'
 },
   function(accessToken, refreshToken, profile, done) {
     User.findOne({
