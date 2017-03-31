8bf18cb09530b223e42931123f07ec34c3159f97
Kyle Anson <kyle.anson7@gmail.com>
2017-03-31 01:54:07 -0400

    Refactor app to make use of ngRoute

--- client/app/app.js
+++ client/app/app.js
@@ -11,8 +11,19 @@ angular.module('hackSource', [
   'hackSource.search',
   'hackSource.user',
   'infinite-scroll',
-  '720kb.socialshare'
+  '720kb.socialshare',
+  'ngRoute'
   ])
+.config(function($routeProvider) {
+  $routeProvider
+  .when('/', {
+    controller: 'cardListCtrl',
+    templateUrl: 'app/cardList/cardList.html'
+  })
+  .otherwise({
+    redirectTo: '/'
+  });
+})
 .run(function() {
   console.log('Moving path of app');
 });
