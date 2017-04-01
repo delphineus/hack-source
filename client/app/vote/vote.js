2797f4f6fb0c5a3985e0011522ed6a22381d7347
Simon de Moor <simondemoor0@gmail.com>
2017-03-31 19:25:44 -0700

    prebase

--- client/app/vote/vote.js
+++ client/app/vote/vote.js
@@ -43,6 +43,14 @@ angular.module('hackSource.vote', [])
 .controller('VoteCtrl', function($scope, counter, User) {
 	$scope.flagVariable = false;
 	var userId;
+	$scope.slider = {
+    value: 50,
+    options: {
+      floor: 0,
+      ceil: 100,
+      readOnly: true
+    }
+  };
 	var resourceId = $scope.resource.id
 	User.checkLoggedIn().then(function(user) {
 		if (user.user.id === undefined) {
